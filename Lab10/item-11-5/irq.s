8454aee4f5fdd1790ff8a7422bf3283f02bda471
Marcos Chen <marcostcchen@gmail.com>
2020-07-23 19:53:25 -0300

    Lab10 all done

--- Lab10/item-11-5/irq.s
+++ Lab10/item-11-5/irq.s
@@ -2,14 +2,13 @@
 .text
 _start:
     b _Reset @posição 0x00 -Reset
-    ldr pc, _undefined_instruction@posição 0x04 -Intrução não-definida
-    ldr pc, _undefined_instruction@posição 0x04 -Intrução não-definida
-    ldr pc, _software_interrupt@posição 0x08 -Interrupção de Software
-    ldr pc, _prefetch_abort@posição 0x0C -Prefetch Abort
-    ldr pc, _data_abort@posição 0x10 -Data Abort
-    ldr pc, _not_used@posição 0x14 -Não utilizado
-    ldr pc, _irq@posição 0x18 -Interrupção (IRQ)
-    ldr pc, _fiq@posição 0x1C -Interrupção(FIQ)
+    ldr pc, _undefined_instruction @posição 0x04 -Intrução não-definida
+    ldr pc, _software_interrupt @posição 0x08 -Interrupção de Software
+    ldr pc, _prefetch_abort @posição 0x0C -Prefetch Abort
+    ldr pc, _data_abort @posição 0x10 -Data Abort
+    ldr pc, _not_used @posição 0x14 -Não utilizado
+    ldr pc, _irq @posição 0x18 -Interrupção (IRQ)
+    ldr pc, _fiq @posição 0x1C -Interrupção(FIQ)
 
 _undefined_instruction: .word undefined_instruction
 _software_interrupt: .word software_interrupt
@@ -28,6 +27,13 @@ TIMER0C:.word 0x101E2008 @timer 0 control register
 TIMER0X:.word 0x101E200c @timer 0 interrupt clear register
 
 _Reset:
+	LDR sp, =svc_stack_top
+
+    MRS r0, cpsr @ salvando o modo corrente em R0
+    MSR cpsr_ctl, #0b11010010 @ alterando o modo para IRQ - o SP eh automaticamente chaveado ao chavear o modo
+    LDR sp, =irq_stack_top @ a pilha de IRQ eh setada 
+    MSR cpsr, r0 @ volta para o modo anterior 
+
     bl  main
     b   .
 
@@ -58,15 +64,15 @@ do_software_interrupt:
     mov pc, r14 @volta p/ o endereço armazenado em r14
     
 @Rotina de interrupções IRQ    
-do_irq_interrupt: 
-    STMFD sp!, {r0 -r3, LR}  @Empilha os registradores
-    LDR r0, INTPND @Carrega o registrador de status de interrupção
-    LDR r0, [r0]
-    
-    TST r0, #0x0010 @verifica se é uma interupção de timer
-    BNE handler_timer @vai para o rotina de tratamento da interupção de timer
-    LDMFD   sp!, {r0 -r3,lr} @retorna
-    mov pc, r14
+do_irq_interrupt: @Rotina de interrupções IRQ
+   STMFD sp!, {r0 - r3, LR}
+   @Empilha os registradores
+   LDR r0, INTPND @Carrega o registrador de status de interrupção 
+   LDR r0, [r0]
+   TST r0, #0x0010 @verifica se é uma interupção de timer
+   BLNE handler_timer @vai para o rotina de tratamento da interupção de timer
+   LDMFD sp!,{R0-R12,pc}^
+   mov pc, r14    @retorna
 
 handler_timer:
     LDR r0, TIMER0X
@@ -74,27 +80,28 @@ handler_timer:
     STR r1, [r0] @Escreve no registrador TIMER0X para limpar o pedido de interrupção
     
     @ Inserir código que seraexecutado na interrupção de timer aqui (chaveamento de processos,  ou alternar LED por exemplo)
-    LDMFD   sp!, {r0 -r3,lr}
+    LDMFD sp!,{R0-R12,pc}^
     mov pc, r14@retorna
 
 timer_init:
-    mrs r0, cpsr
-    bic r0,r0,#0x80
-    msr cpsr_c, r0 @enabling interrupts in the cpsr
-    LDR r0, INTEN
-    LDR r1,=0x10 @bit 4 for timer 0 interrupt enable
-    STR r1,[r0]
-    LDR r0, TIMER0C
-    LDR r1, [r0]
-    MOV r1, #0xA0 @enable timer module
-    STR r1, [r0]
-    LDR r0, TIMER0V 
-    MOV r1, #0xff @setting timer value
-    STR r1,[r0]
-    mov pc, lr
-
+ LDR r0, INTEN
+ LDR r1,=0x10 @bit 4 for timer 0 interrupt enable
+ STR r1,[r0]
+ LDR r0, TIMER0L
+ LDR r1, =0xffffff @setting timer value
+ STR r1,[r0]
+ LDR r0, TIMER0C
+ MOV r1, #0xE0 @enable timer module
+ STR r1, [r0]
+ mrs r0, cpsr
+ bic r0,r0,#0x80
+ msr cpsr_c,r0 @enabling interrupts in the cpsr
+ mov pc, lr
+ 
 main:
     bl timer_init @initialize interrupts and timer 0
 
 stop:
+    
     b stop
+
---
+++ Lab10/item-11-6/handler.c
@@ -0,0 +1,24 @@
+volatile unsigned int * const UART0DR = (unsigned int *)0x101f1000;
+
+int print_uart0(const char *s) {
+   while(*s != '\0') { /* Loop until end of string */
+   *UART0DR = (unsigned int)(*s); /* Transmit char */
+   s++; /* Next char */
+   }
+return 0;
+}
+ 
+int handler() {
+    int *TIMER0X;
+    TIMER0X = 0x101E200c;
+    *TIMER0X = 0;
+
+    print_uart0("#");   
+    
+    return 0;
+}
+
+int printSpaces() {
+    print_uart0(" "); 
+    return 0;  
+}
