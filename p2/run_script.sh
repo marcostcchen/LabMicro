c21a69b6362d4c1245a4e90297391e4e5cd11e3d
Marcos Chen <marcostcchen@gmail.com>
2020-08-03 11:51:04 -0300

    LabMicro P2 preparation all done

---
+++ p2/run_script.sh
@@ -0,0 +1,4 @@
+arm-none-eabi-gcc -c -mcpu=arm926ej-s -Wall -Wextra -g handler.c -o handler.o
+arm-none-eabi-as -c -mcpu=arm926ej-s -g irq.s -o irq.o
+arm-none-eabi-ld -T irqld.ld irq.o handler.o -o irq.elf
+qemu-system-arm -M versatilepb -m 128M -nographic -kernel irq.elf -s -S
